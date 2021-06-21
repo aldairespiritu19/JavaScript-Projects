64610ab17f16d910c40c61ce674eccae6225506f
aldairespiritu19 <aldairespiritu19@yahoo.com>
2021-06-21 12:36:54 -0700

    pushing folder

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+var X=("Hello, world!");
+var X= X.fontcolor("pink"); //Making the font color pink
+document.write(X); 
+
+var A="This is a string";
+var A= A.fontcolor("red"); //Making the font color red
+document.write(A);
+
+var Y=("Jim told Ben, \"Don't go outside right now!\"<br> \"I'm still going!\"Ben responded.");
+var Y= Y.fontcolor("orange"); //Making the font color orange
+document.write(Y);
+
+var D=("\"Do whatever you want to do,"+"because at the end who\'s there for you?\""+"-Unknown");
+var D= D.fontcolor("cyan"); //Making the font color cyan
+document.write(D);
+
+var B="Concatenated"+"String";
+var B= B.fontcolor("purple"); //Making the font color purple
+document.write(B);
+
+var Team_Players="NBA", Lakers="Lebron", Blazers="Lillard", Sixers="Simmons", Hawks="Trae"; //Giving each variable a value
+var Lakers= Lakers.fontcolor("green"); //Making the font color green
+document.write(Lakers);
+
+document.write(10 + 5); //An expression adding two numbers
